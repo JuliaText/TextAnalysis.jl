7cf5dc5986d2a16db69957fb02bba0c280d8fee2
amartinez <amartinez@daedalus.es>
2014-10-24 12:58:55 +0200

    Added tf_idf methods that where throwing "not implemented" errors, added tests for tf_idf

--- src/tf_idf.jl
+++ src/tf_idf.jl
@@ -4,17 +4,32 @@
 #
 ##############################################################################
 
-function tf_idf{T <: Real}(dtm::Matrix{T})
-    n, p = size(dtm)
+tf_idf{T <: Real}(dtm::Matrix{T}) = tf_idf!(dtm, Array(Float64, size(dtm)...))
 
+tf_idf{T <: Real}(dtm::SparseMatrixCSC{T}) =  tf_idf!(dtm, sparse(Int[], Int[], 0.0, size(dtm)...))
+
+tf_idf!{T <: Real}(dtm::AbstractMatrix{T}) = tf_idf!(dtm, dtm)
+
+tf_idf!{T <: Real}(dtm::SparseMatrixCSC{T}) = tf_idf!(dtm, dtm)
+
+tf_idf(dtm::DocumentTermMatrix) = tf_idf(dtm.dtm)
+
+# This does not make sense, since DocumentTermMatrix is based on an array of integers
+#tf_idf!(dtm::DocumentTermMatrix) = tf_idf!(dtm.dtm)
+
+
+# The second Matrix will be overwritten with the result
+# Will work correctly if dtm and tfidf are the same matrix
+function tf_idf!{T1 <: Real, T2 <: FloatingPoint}(dtm::AbstractMatrix{T1}, tfidf::AbstractMatrix{T2})
+    n, p = size(dtm)
+    
     # TF tells us what proportion of a document is defined by a term
-    tf = Array(Float64, n, p)
     for i in 1:n
         words_in_document = 0
         for j in 1:p
             words_in_document += dtm[i, j]
         end
-        tf[i, :] = dtm[i, :] ./ words_in_document
+        tfidf[i, :] = dtm[i, :] ./ words_in_document
     end
 
     # IDF tells us how rare a term is in the corpus
@@ -22,57 +37,11 @@ function tf_idf{T <: Real}(dtm::Matrix{T})
     idf = log(n ./ documents_containing_term)
 
     # TF-IDF is the product of TF and IDF
-    # We store it in the TF matrix to save space
     for i in 1:n
         for j in 1:p
-           tf[i, j] = tf[i, j] * idf[j]
+           tfidf[i, j] = tfidf[i, j] * idf[j]
         end
     end
 
-    return tf
-end
-
-# TODO: Rewrite this definition to return a sparse matrix
-function tf_idf{T <: Real}(dtm::SparseMatrixCSC{T})
-    n, p = size(dtm)
-
-    # TF tells us what proportion of a document is defined by a term
-    tf = Array(Float64, n, p)
-    for i in 1:n
-        words_in_document = 0
-        for j in 1:p
-            words_in_document += dtm[i, j]
-        end
-        tf[i, :] = dtm[i, :] ./ words_in_document
-    end
-
-    # IDF tells us how rare a term is in the corpus
-    documents_containing_term = vec(sum(dtm .> 0, 1))
-    idf = log(n ./ documents_containing_term)
-
-    # TF-IDF is the product of TF and IDF
-    # We store it in the TF matrix to save space
-    for i in 1:n
-        for j in 1:p
-           tf[i, j] = tf[i, j] * idf[j]
-        end
-    end
-
-    return tf
-end
-
-function tf_idf!{T <: Real}(dtm::Matrix{T})
-    error("not yet implemented")
-end
-
-function tf_idf!{T <: Real}(dtm::SparseMatrixCSC{T})
-    error("not yet implemented")
-end
-
-function tf_idf(dtm::DocumentTermMatrix)
-    tf_idf(dtm.dtm)
-end
-
-function tf_idf!(dtm::DocumentTermMatrix)
-    tf_idf!(dtm.dtm)
+    return tfidf
 end
