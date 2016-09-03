aff7d26f7529b9484ad0ad1203e0b977dca6ac84
Avik Sengupta <avik@sengupta.net>
2016-09-04 00:16:04 +0100

    fix build on 0.5

--- deps/build.jl
+++ deps/build.jl
@@ -1,4 +1,5 @@
 using BinDeps
+using Compat
 
 @BinDeps.setup
 
@@ -20,12 +21,12 @@ for path in [prefix, dnlddir, srchome, bindir]
 end
 
 if !isfile(binpath)
-    @unix_only begin
+    if is_unix()
         run(download_cmd("http://snowball.tartarus.org/dist/snowball_code.tgz",dnldfile))
         cd(srchome)
         run(`tar xvzf $dnldfile`)
         cd(srcdir)
-        run(`cat $patchpath` |> `patch`)
+        run(pipeline(`cat $patchpath`, `patch`))
         for mkcmd in (:gnumake, :gmake, :make)
             try
                 if success(`$mkcmd`)
@@ -38,11 +39,10 @@ if !isfile(binpath)
         end
     end
 
-    @windows_only begin
+    if is_windows()
         Base.warn("No integrated stemmer available on Windows yet.\n" *
                   "Place a compiled Snowball stemmer dll at $binpath for stemming to work.")
     end
 end
 
 provides(Binaries, bindir, libstemmer)
-
