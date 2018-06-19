0f542c9699b43b308424d5369caf1d0a350d96ad
ayush1999 <ayush.shridhar1999@gmail.com>
2018-06-19 16:39:31 +0530

    Generated code

--- src/sentiment.jl
+++ src/sentiment.jl
@@ -1,6 +1,6 @@
-using Keras 
 using JSON
 using Flux
+using HDF5
 
 const deps = joinpath(@__DIR__, "..", "deps")
 const deps_sentiment = joinpath(deps, "Sentiment Analysis")
@@ -17,15 +17,38 @@ function pad_sequences(l, maxlen=500)
         return res
     end
 end
-    
+
+function read_weights(filename=deps_sentiment_weight)
+    return read(h5open(filename))
+end
+
 function read_word_ids(filename=deps_sentiment_ids)
     return JSON.parse(String(read(open(filename, "r"))))
 end
 
 get_op(x) = x>=0.5?1:0
 
+function embedding(embedding_matrix, x)
+    temp = embedding_matrix[:, Int64(x[1])+1]
+    for i=2:length(x)
+        temp = hcat(temp, embedding_matrix[:, Int64(x[i])+1])
+    end
+    return reshape(temp, reverse(size(temp)))
+end
+
+function flatten(x)
+    return vec(x)
+end
+
 function get_sentiment(text)
-    model = Keras.load(deps_sentiment_structure, deps_sentiment_weight)
+    weight = read_weights(deps_sentiment_weight)
+    model = (x,) -> begin
+    a_1 = embedding(weight["embedding_1"]["embedding_1"]["embeddings:0"], x)
+    a_2 = flatten(a_1)
+    a_3 = Dense(weight["dense_1"]["dense_1"]["kernel:0"], weight["dense_1"]["dense_1"]["bias:0"], relu)(a_2)
+    a_4 = Dense(weight["dense_2"]["dense_2"]["kernel:0"], weight["dense_2"]["dense_2"]["bias:0"], sigmoid)(a_3)
+    return a_4
+    end
     rwi = read_word_ids(deps_sentiment_ids)
     ip = split(text, " ")
     res = Array{Any, 1}()
