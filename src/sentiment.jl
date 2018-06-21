9e8a470e2160b2fe675dc0099a5bde4de875b501
Avik Sengupta <avik@sengupta.net>
2018-06-22 00:38:15 +0100

    Use BinaryProvider to manage model download. Use BSON.

--- src/sentiment.jl
+++ src/sentiment.jl
@@ -1,12 +1,6 @@
 using JSON
 using Flux
-using HDF5
-
-const deps = joinpath(@__DIR__, "..", "deps")
-const deps_sentiment = joinpath(deps, "Sentiment Analysis")
-const deps_sentiment_structure = joinpath(deps_sentiment, "sentiment-analysis-structure.json")
-const deps_sentiment_weight = joinpath(deps_sentiment, "sentiment-analysis-weights.h5")
-const deps_sentiment_ids = joinpath(deps_sentiment, "sentiment-analysis-word-to-id.json")
+using BSON
 
 function pad_sequences(l, maxlen=500)
     if length(l) <= maxlen
@@ -18,11 +12,11 @@ function pad_sequences(l, maxlen=500)
     end
 end
 
-function read_weights(filename=deps_sentiment_weight)
-    return read(h5open(filename))
+function read_weights(filename=sentiment_weights)
+    return BSON.load(filename)
 end
 
-function read_word_ids(filename=deps_sentiment_ids)
+function read_word_ids(filename=sentiment_words)
     return JSON.parse(String(read(open(filename, "r"))))
 end
 
@@ -41,15 +35,15 @@ function flatten(x)
 end
 
 function get_sentiment(text)
-    weight = read_weights(deps_sentiment_weight)
+    weight = read_weights(sentiment_weights)
     model = (x,) -> begin
-    a_1 = embedding(weight["embedding_1"]["embedding_1"]["embeddings:0"], x)
+    a_1 = embedding(weight[:embedding_1]["embedding_1"]["embeddings:0"], x)
     a_2 = flatten(a_1)
-    a_3 = Dense(weight["dense_1"]["dense_1"]["kernel:0"], weight["dense_1"]["dense_1"]["bias:0"], relu)(a_2)
-    a_4 = Dense(weight["dense_2"]["dense_2"]["kernel:0"], weight["dense_2"]["dense_2"]["bias:0"], sigmoid)(a_3)
+    a_3 = Dense(weight[:dense_1]["dense_1"]["kernel:0"], weight[:dense_1]["dense_1"]["bias:0"], relu)(a_2)
+    a_4 = Dense(weight[:dense_2]["dense_2"]["kernel:0"], weight[:dense_2]["dense_2"]["bias:0"], sigmoid)(a_3)
     return a_4
     end
-    rwi = read_word_ids(deps_sentiment_ids)
+    rwi = read_word_ids(sentiment_words)
     ip = split(text, " ")
     res = Array{Any, 1}()
     for ele in ip
@@ -62,19 +56,5 @@ struct SentimentAnalyser
 end
 
 function(m::SentimentAnalyser)(text)
-    cd(deps)
-    if "Sentiment Analysis" in readdir()
-        return get_sentiment(text)
-    else
-        mkdir("Sentiment Analysis")
-        cd("Sentiment Analysis")
-        download("https://github.com/ayush1999/TextAnalysis.jl/releases/download/v0.3.0/sentiment-analysis-structure.json",
-        "sentiment-analysis-structure.json")
-        download("https://github.com/ayush1999/TextAnalysis.jl/releases/download/v0.3.0/sentiment-analysis-weights.h5",
-        "sentiment-analysis-weights.h5")
-        download("https://github.com/ayush1999/TextAnalysis.jl/releases/download/v0.3.0/sentiment-analysis-word-to-id.json",
-        "sentiment-analysis-word-to-id.json")
-        return get_sentiment(text)
-    end
+    return get_sentiment(text)
 end
-
