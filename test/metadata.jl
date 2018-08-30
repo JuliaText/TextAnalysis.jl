36a94ed730192a3d4f594495995a4b6673afa57f
laschuet <laschuet@users.noreply.github.com>
2018-08-30 17:39:22 +0200

    Complete tests for the document metadata setters

--- test/metadata.jl
+++ test/metadata.jl
@@ -7,11 +7,19 @@
 
     sd = StringDocument(sample_text1)
 
+    # Single document metadata getters
     @test isequal(name(sd), "Unnamed Document")
     @test isequal(language(sd), Languages.English())
     @test isequal(author(sd), "Unknown Author")
     @test isequal(timestamp(sd), "Unknown Time")
 
+    # Single document metadata setters
+    name!(sd, "Document")
     language!(sd, Languages.German())
+    author!(sd, "Author")
+    timestamp!(sd, "Time")
+    @test isequal(name(sd), "Document")
     @test isequal(language(sd), Languages.German())
+    @test isequal(author(sd), "Author")
+    @test isequal(timestamp(sd), "Time")
 end
