777d25f346a53b568a9105882f152ead096c5fa4
Samriddhi Sinha <samriddhidjokestersinha@gmail.com>
2019-05-04 23:53:30 +0530

    Created utils.jl and Added jackknife function
    Created utils.jl and Added jackknife function for averaging to be used in ROUGE score

---
+++ src/utils.jl
@@ -0,0 +1,27 @@
+function jackknife_avg(scores)
+
+    #= The jackknife is a resampling technique especially useful for variance and bias estimation. 
+    Currently being used for averaging in ROUGE scores in evaluate.jl
+    :param scores: List of integers to average
+    :type scores: Array{Int64,1} =#
+    
+    if length(collect(Set(scores))) == 1
+    
+        #= In case the elements of the array are all equal=#
+        return scores[1]
+        
+    else
+    
+        #=store the maximum scores
+        from the m different sets of m-1 scores.
+        such that m is the len(score_list)=#
+        
+        average = []
+        for i in scores
+            # dummy : list a particular combo of m-1 scores
+            dummy = [j for j in scores if i != j]
+            append!(average, max(dummy...))
+            end
+        return sum(average)/length(average)
+    end
+end
