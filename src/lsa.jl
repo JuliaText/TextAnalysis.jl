606a5a980c4c6dd95cee5fb65abf2cca5936c7b3
aquatiko <rohit970819@gmail.com>
2019-05-12 12:48:07 +0530

    doc for lsa.jl

--- src/lsa.jl
+++ src/lsa.jl
@@ -3,6 +3,16 @@
 # LSA
 #
 ##############################################################################
+"""
+```
+lsa(crps)
+lsa(dtm)
+```
+Performs Latent Semantic Analysis or LSA on a corpus.
 
+Parameters:
+	-  cprs	    = A Corpus type object
+	-  dtm	    = A DocumentTermMatrix object
+"""
 lsa(dtm::DocumentTermMatrix) = svd(tf_idf(dtm))
 lsa(crps::Corpus) = svd(tf_idf(DocumentTermMatrix(crps)))
