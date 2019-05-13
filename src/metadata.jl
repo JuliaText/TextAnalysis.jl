1209ef05f5324186a890bf2b49a80fce9f8b1b65
Ayushk4 <ayushk4@gmail.com>
2019-05-13 22:08:20 +0530

    Add docstrings for metadata.jl

--- src/metadata.jl
+++ src/metadata.jl
@@ -1,41 +1,107 @@
-##############################################################################
-#
-# Metadata field getters and setters
-#
-##############################################################################
-
 import Languages.name
 
+"""
+    title(doc)
+
+Returns the title metadata for `doc`
+"""
 title(d::AbstractDocument) = d.metadata.title
+
+"""
+    language(doc)
+
+Returns the language metadata for `doc`
+"""
 language(d::AbstractDocument) = d.metadata.language
+
+"""
+    author(doc)
+
+Returns the author metadata for `doc`
+"""
 author(d::AbstractDocument) = d.metadata.author
+
+"""
+    timestamp(doc)
+
+Returns the timestamp metadata for `doc`
+"""
 timestamp(d::AbstractDocument) = d.metadata.timestamp
 
+
+"""
+    title!(doc, str)
+
+Sets the title of `doc` to `str`
+"""
 function title!(d::AbstractDocument, nv::AbstractString)
     d.metadata.title = nv
 end
 
+"""
+    language!(doc, lang)
+
+Sets the language of `doc` to `lang`.
+
+# Example
+```julia-repl
+julia> d = StringDocument("String Document 1")
+
+julia> language!(d, Languages.Spanish())
+
+julia> d.metadata.language
+Languages.Spanish()
+```
+"""
 function language!(d::AbstractDocument, nv::T) where T <: Language
     d.metadata.language = nv
 end
 
+"""
+    author!(doc, authr)
+
+Sets the `author` metadata of doc to `authr`
+"""
 function author!(d::AbstractDocument, nv::AbstractString)
     d.metadata.author = nv
 end
 
+"""
+    author!(doc, timestmp)
+
+Sets the `timestamp` metadata of doc to `timestmp`
+"""
 function timestamp!(d::AbstractDocument, nv::AbstractString)
     d.metadata.timestamp = nv
 end
 
-##############################################################################
-#
-# Vectorized getters for an entire Corpus
-#
-##############################################################################
 
+"""
+    titles(crps)
+
+Return the titles for each document in `crps`
+"""
 titles(c::Corpus) = map(d -> title(d), documents(c))
+
+"""
+    languages(crps)
+
+Return the languages for each document in `crps`
+"""
 languages(c::Corpus) = map(d -> language(d), documents(c))
+
+"""
+    authors(crps)
+
+Return the authors for each document in `crps`
+"""
 authors(c::Corpus) = map(d -> author(d), documents(c))
+
+"""
+    timestamps(crps)
+
+Return the timestamps for each document in `crps`
+"""
 timestamps(c::Corpus) = map(d -> timestamp(d), documents(c))
 
 titles!(c::Corpus, nv::AbstractString) = title!.(documents(c), nv)
@@ -43,6 +109,14 @@ languages!(c::Corpus, nv::T) where {T <: Language} = language!.(documents(c), Re
 authors!(c::Corpus, nv::AbstractString) = author!.(documents(c), Ref(nv))
 timestamps!(c::Corpus, nv::AbstractString) = timestamp!.(documents(c), Ref(nv))
 
+"""
+    titles!(crps, ::Vector{String})
+    titles!(crps, str)
+
+Updates the titles of the documents in `crps` to the strings in the input vector.
+
+See also: [`title!`](@ref), [`titles`](@ref)
+"""
 function titles!(c::Corpus, nvs::Vector{String})
     length(c) == length(nvs) || throw(DimensionMismatch("dimensions must match"))
     for (i, d) in pairs(IndexLinear(), documents(c))
@@ -50,6 +124,12 @@ function titles!(c::Corpus, nvs::Vector{String})
     end
 end
 
+"""
+    languages!(crps, langs)
+    languages!(crps, lang)
+
+Updates the languages of the documents in `crps` to `langs`, respectively.
+"""
 function languages!(c::Corpus, nvs::Vector{T}) where T <: Language
     length(c) == length(nvs) || throw(DimensionMismatch("dimensions must match"))
     for (i, d) in pairs(IndexLinear(), documents(c))
@@ -57,6 +137,12 @@ function languages!(c::Corpus, nvs::Vector{T}) where T <: Language
     end
 end
 
+"""
+    authors!(crps, athrs)
+    authors!(crps, athr)
+
+Sets the authors of the documents in `crps` to the `athrs`, respectively.
+"""
 function authors!(c::Corpus, nvs::Vector{String})
     length(c) == length(nvs) || throw(DimensionMismatch("dimensions must match"))
     for (i, d) in pairs(IndexLinear(), documents(c))
@@ -64,6 +150,12 @@ function authors!(c::Corpus, nvs::Vector{String})
     end
 end
 
+"""
+    timestamps!(crps, times)
+    timestamps!(crps, time)
+
+Sets the timestamps of the documents in `crps` to the timestamps in `times`, respectively .
+"""
 function timestamps!(c::Corpus, nvs::Vector{String})
     length(c) == length(nvs) || throw(DimensionMismatch("dimensions must match"))
     for (i, d) in pairs(IndexLinear(), documents(c))
