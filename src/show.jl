07b9c0c370f4638c1d43a9a3425df6395b2fdd2c
Ayushk4 <ayushk4@gmail.com>
2019-05-13 18:24:54 +0530

    Fix depracations in show.jl

--- src/show.jl
+++ src/show.jl
@@ -1,9 +1,3 @@
-##############################################################################
-#
-# Basic printing of TextAnalysis types
-#
-##############################################################################
-
 show(io::IO, d::AbstractDocument) = print(io, "A $(typeof(d))")
 show(io::IO, crps::Corpus) = print(io, "A Corpus")
 show(io::IO, dtm::DocumentTermMatrix) = print(io, "A DocumentTermMatrix")
@@ -21,10 +15,11 @@ function summary(d::AbstractDocument)
     o *= " * Title: $(title(d))\n"
     o *= " * Author: $(author(d))\n"
     o *= " * Timestamp: $(timestamp(d))\n"
-    if contains(Any[TokenDocument, NGramDocument], typeof(d))
+
+    if typeof(d) ∈ [TokenDocument, NGramDocument]
         o *= " * Snippet: ***SAMPLE TEXT NOT AVAILABLE***"
     else
-        sample_text = replace(text(d)[1:50], r"\s+", " ")
+        sample_text = replace(text(d)[1:min(50, length(text(d)))], r"\s+", " ")
         o *= " * Snippet: $(sample_text)"
     end
     return o
@@ -51,12 +46,6 @@ function summary(dtm::DocumentTermMatrix)
     n, p = size(dtm.dtm)
     @sprintf "A %dx%d DocumentTermMatrix" n p
 end
-
-##############################################################################
-#
-# In the REPL, show the summary by default
-#
-##############################################################################
 
 repl_show(io::IO, d::AbstractDocument) = print(io, summary(d))
 repl_show(io::IO, crps::Corpus) = print(io, summary(crps))
