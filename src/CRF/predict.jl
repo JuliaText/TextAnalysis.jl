af391acb1f23a1681da63b4e2416247463412459
Ayushk4 <ayushk4@gmail.com>
2019-06-19 11:06:03 +0530

    Fix viterbi

--- src/CRF/predict.jl
+++ src/CRF/predict.jl
@@ -6,7 +6,7 @@ Probabilities for the first tag in the tagging sequence.
 """
 function preds_first(a::CRF, x)
     s, f = a.s, a.f
-    f(x) * s
+    dot.(f(x), s)
 end
 
 """
@@ -17,57 +17,61 @@ function preds_single(a::CRF, x)
     f(x) * W + b
 end
 
-# """
-# Compute the (normalized) probability of
-# a particular label sequence for given inputs sequence.
-# """
-# function label_sequence_pred(a::CRF, x_seq, label_seq)
-#     length(x_seq) == length(label_seq) || throw("Lengths of input sequence and labels, do not match")
-#     length(x_seq) == 0 && throw("Lengths of input sequence is zero")
-#
-#     forward_mat = zeros(length(x_seq), size(a.s, 2))
-#     forward_mat[1] = preds_firstLinearAlgebra(a, x_seq[1])
-#
-#      for i in 2:length(x_seq)
-#         forward_mat[i,:] = log_sum_exp(preds_single(a,x_seq[i]) + forward_mat[i-1])
-#     end
-# end
-
+# TODO: Parallel
 # Helper for forward pass, returns max_probs and corresponding arg_max for all the labels
 function forward_unit_max(a::CRF, x, prev)
-    preds = preds_single(a, x)
+    preds = log_sum_exp(preds_single(a, x) .+ prev)
 
     max_values = zeros(length(prev))
     label_indices = zeros(length(prev))
 
-    for j in range(1, step=length(prev), size(a.W, 2))
-        k = preds[j:j + length(prev) - 1]
+    for j in 1:length(prev)
+        k = preds[:, j:j + length(prev) - 1]
         i = ceil(j/length(prev))
-        max_values[i], label_indices[i] = findmax(maxprev .* k)
+
+        max_values[i], label_indices[i] = findmax()
     end
-    return max_values, label_indices
+
+    return log_sum_exp(max_values), label_indices
 end
 
-function forward_pass(a::CRF, x_seq)
-    α_val = zeros(length(x_seq), size(a.s, 2))
-    α_max = zeros(length(x_seq), size(a.s, 2))
+"""
+Computes the forward pass for viterbi algorithm.
+"""
+function forward_pass(a::CRF, x)
+    α_val = log_sum_exp(preds_first(a, x[1, :]))
+    α_idx = zeros(size(x, 1), size(a.s, 2))
 
-    α_val[1,:] = findmax(preds_first(a, x_seq[1])
+    for i in 2:size(x, 1)
+        α_val, α_idx[i,:] = forward_unit_max(a, x[i, :], α_val)
+    end
 
-    for i in 2:length(x_seq)
-        α_val[i,:], α_max[i,:] = forward_unit_max(a, x[i], α_1[i-1,:])
+    return findmax(α_val)[2], α_idx
+end
+
+# TODO: Speeding up.
+"""
+Computes the forward pass for viterbi algorithm.
+"""
+function backward_pass(a::CRF, (α_idx_last, α_idx))[
+    labels = zeros(size(α_idx,1))
+    labels[end] = α_idx_last
+
+    for i in reverse(1:size(α_idx,1) - 1)
+        labels[i] = α_idx[labels[i + 1]]
     end
 end
 
-function backward_pass(a::CRF, α_val, α_max)
+"""
+    viterbi_decode(::CRF, input_sequence)
 
-    # for last element, simply look up α
-
+Predicts the most probable label sequence of `input_sequence`.
+"""
+function viterbi_decode(a::CRF, x_seq::Array{Number, 2})
+    size(x_seq,1) == 0 && throw("Input sequence is empty")
+    α_star, α_max = backward_pass(a, forward_pass(a, x_seq))
 end
-"""
-Predict the most probable label sequence.
-"""
-function viterbi_decode(a::CRF, x_seq)
-    length(x_seq) == 0 && throw("Input sequence is empty")
-    α_star, α_max = backward_pass(a, forward_pass(a,x_seq))
+
+function predict(a::CRF, x_seq)
+    viterbi_decode(a, x_seq)
 end
