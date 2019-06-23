3fcb1725ac0e854f20e6c99f2da44184b49dc746
Ayushk4 <ayushk4@gmail.com>
2019-06-23 20:17:53 +0530

    Basic tests for CRF

--- test/crf.jl
+++ test/crf.jl
@@ -1,3 +1,78 @@
-@testset begin
-    a = CRF(10, 30)
-end
+using Flux: onehot, train!, Params, gradient
+using Flux
+using TextAnalysis: CRF
+# @testset "crf" begin
+
+    path = "data/weather.csv"
+    function load(path::String)
+        stream = open(path, "r")
+        Xs = []
+        Ys = []
+        xs = []
+        ys = Array{String,1}()
+
+        for line in map(strip, eachline(stream))
+            if isempty(line)
+                push!(Xs, hcat(xs...))
+                push!(Ys, ys)
+                xs = []
+                ys = []
+            else
+                x1, x2, y = split(line, ',')
+                push!(xs, [parse(Float64, x1), parse(Float64, x2) ])
+                push!(ys, y)
+            end
+        end
+        if length(xs) != 0
+            push!(Xs, hcat(xs...))
+            push!(Ys, ys)
+        end
+
+        close(stream)
+        return Xs, Ys
+    end
+
+    X, Y = load(path)
+
+    labels = unique(Iterators.flatten(Y))
+    num_labels = length(labels)
+    num_features = size(X[1], 1)
+
+    Y = map.(ch -> onehot(ch, labels), Y)
+
+    train_X = X[1:10]
+    train_Y = Y[1:10]
+
+    m = CRF(num_labels, num_features)
+
+    loss(x, y) = crf_loss(m, x, y) # TODO: change this to loss = crf_loss(m)
+
+    opt = ADAM(0.01)
+
+    Flux.train!(loss, params(m), zip(train_X, train_Y), opt)
+    # function train!(loss, ps, data, opt; cb = () -> ())
+    #     ps = Params(ps)
+    #     @progress for d in data
+    #         try
+    #             gs = gradient(ps) do
+    #                 loss(d...)
+    #             end
+    #             update!(opt, ps, gs)
+    #             if cb() == :stop
+    #                 depwarn("Use of `:stop` is deprecated; use `Flux.stop()` instead", :stop)
+    #                 break
+    #             end
+    #         catch ex
+    #             if ex isa StopException
+    #                 break
+    #             else
+    #                 rethrow(ex)
+    #             end
+    #         end
+    #     end
+    # end
+
+    train!()
+# end
+
+# TODO: sequence of varying lengths.
