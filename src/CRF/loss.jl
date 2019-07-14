6f6106cf5bdf842215c8bad40e54285249a97611
Ayushk4 <ayushk4@gmail.com>
2019-07-14 18:02:45 +0530

    fix loss function

--- src/CRF/loss.jl
+++ src/CRF/loss.jl
@@ -7,7 +7,7 @@ function forward_algorithm_stable(c::CRF, x)
         log_α_forward = log_sum_exp(log_α_forward .+ preds_single(c, x[i])')
     end
 
-    return log_α_forward
+    return log(sum(exp.(log_α_forward)))
 end
 
 function forward_algorithm(c::CRF, x)
@@ -17,7 +17,7 @@ function forward_algorithm(c::CRF, x)
         log_α_forward = log_α_forward * exp.(preds_single(c, x[i]))
     end
 
-    return log_α_forward
+    return sum(log_α_forward)
 end
 # Calculating the score of the desired label_seq against input_seq.
 # Not exponentiated as required for negative log likelihood,
@@ -38,5 +38,5 @@ end
 The partition function is needed to reduce the score_sequence
 to probabilities ( b/w 0 and 1 )
 """
-crf_loss(c::CRF, input_seq, label_seq) = forward_algorithm(c, input_seq) -
+crf_loss(c::CRF, input_seq, label_seq) = log(forward_algorithm(c, input_seq)) -
                                          score_sequence(c, input_seq, label_seq)
