d3f92ef45f770d205fc3b58303401a838166e90e
Ayushk4 <ayushk4@gmail.com>
2019-07-18 07:44:25 +0530

    Update loss and forward algo as per new CRF struct

--- src/CRF/loss.jl
+++ src/CRF/loss.jl
@@ -1,58 +1,39 @@
-using Flux: onecold
-using CUDAnative
+"""
+    forward_score(c::CRF, x::Array)
 
-# Normalization / partition function / Forward Algorithm score - `Z`
-
-function forward_algorithm_stable(c::CRF, x)
-    log_α_forward = preds_first(c, x[1])
-
-    for i in 2:length(x)
-        log_α_forward = log_sum_exp(log_α_forward .+ preds_single(c, x[i])')
-    end
-
-    return log(sum(exp.(log_α_forward)))
-end
-
-function forward_algorithm_gpu(c::CRF, x)
-    log_α_forward = CUDAnative.exp.(preds_first(c, x[1]))
+Compute the Normalization / partition function
+or the Forward Algorithm score - `Z`
+"""
+function forward_score(c::CRF, x, init_α)
+    forward_var = log_sum_exp((c.W .+ x[1]') .+ init_α)
 
     for i in 2:length(x)
-        log_α_forward = log_α_forward * CUDAnative.exp.(preds_single(c, x[i]))
+        forward_var = log_sum_exp((c.W .+ x[i]') .+ forward_var')
     end
 
-    return sum(log_α_forward)
+    return log_sum_exp(c.W[:, c.n + 2] + forward_var')[1]
 end
 
-function forward_algorithm_cpu(c::CRF, x)
-    log_α_forward = exp.(preds_first(c, x[1]))
+"""
+    score_sequence(c::CRF, xs, label_seq)
 
-    for i in 2:length(x)
-        log_α_forward = log_α_forward * exp.(preds_single(c, x[i]))
-    end
+Calculating the score of the desired `label_seq` against sequence `xs`.
+Not exponentiated as required for negative log likelihood,
+thereby preventing operation.
 
-    return sum(log_α_forward)
-end
-# Calculating the score of the desired label_seq against input_seq.
-# Not exponentiated as required for negative log likelihood,
-# thereby preventing operation
-#### Does not return exponentiated score.
-function score_sequence(c::CRF, input_seq, label_seq)
-    score = onecold(label_seq[1], preds_first(c, input_seq[1]))
+`label_seq`<:Array/ CuArray
+eltype(label_seq) = Flux.OneHotVector
+"""
+function score_sequence(c::CRF, x, label_seq)
+    score = preds_first(c, label_seq[1]) + onecold(label_seq[1], x[1])
 
     for i in 2:length(label_seq)
-        score += onecold(label_seq[i-1], preds_single(c, input_seq[i]) * label_seq[i])
+        score += preds_single(c, label_seq[i], label_seq[i-1]) +
+                    onecold(label_seq[i], x[i])
     end
-    return score
+
+    return score + preds_last(c, label_seq[end])
 end
 
-# REGULARIZATION TERM AND EMISSION SCORES
-
-"""
-The partition function is needed to reduce the score_sequence
-to probabilities ( b/w 0 and 1 )
-"""
-crf_loss_gpu(c::CRF, input_seq, label_seq) = log(forward_algorithm_gpu(c, input_seq)) -
-                                                score_sequence(c, input_seq, label_seq)
-
-crf_loss_cpu(c::CRF, input_seq, label_seq) = log(forward_algorithm_cpu(c, input_seq)) -
-                                                score_sequence(c, input_seq, label_seq)
+# REGULARIZATION TERM
+crf_loss(c::CRF, x, label_seq, init_α) = forward_score(c, x, init_α) - score_sequence(c, x, label_seq)
