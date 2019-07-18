3e8342716fb21ed65306a547a8a49f28901c0532
Ayushk4 <ayushk4@gmail.com>
2019-07-18 07:43:15 +0530

    Docstring for stable log_sum_exp

--- src/CRF/crf_utils.jl
+++ src/CRF/crf_utils.jl
@@ -1,4 +1,8 @@
-# More stable implementation as exponentiation creates large nos.
-# where m = maximum(z, dims = 2)
-log_sum_exp(z, m ) = log.(sum(exp.(z .- m), dims = 2)) .+ m
-log_sum_exp(z) = log_sum_exp(z, maximum(z, dims = 2))
+"""
+    log_sum_exp(z::Array)
+
+A stable implementation f(x) = log ∘ sum ∘ exp (x).
+Since exponentiation can lead to very large numbers.
+"""
+log_sum_exp(z) = log_sum_exp(z, maximum(z, dims = 1))
+log_sum_exp(z, m) = log.(sum(exp.(z .- m), dims = 1)) .+ m
