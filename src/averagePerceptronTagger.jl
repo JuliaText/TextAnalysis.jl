d200c7f90d2166e267dfbaa7ff173203377cc99e
Ayushk4 <ayushk4@gmail.com>
2019-08-10 11:51:11 +0530

    Switch to calling register_datadeps instead using include.

--- src/averagePerceptronTagger.jl
+++ src/averagePerceptronTagger.jl
@@ -5,18 +5,20 @@ using DataDeps
 
 export fit!, predict
 
-register(DataDep("POS Perceptron Tagger Weights",
-    """
-    The trained weights for the average Perceptron Tagger on Part of Speech Tagging task.
-    """,
-    "https://github.com/JuliaText/TextAnalysis.jl/raw/2467ae2f379490af9ba1b181ce25f1a415a4be4d/src/pretrainedMod.bson",
-    "3305c8ee73d9de6d653d6a6e4eaf83dc5031114aaebe621b1625f11d7810a5f4",
-    post_fetch_method = function(fn)
-        file = readdir(".")[1]
-        println(readdir("."))
-        mv(file, "POSWeights.bson")
-    end
-))
+function pos_tagger_datadep_register()
+    register(DataDep("POS Perceptron Tagger Weights",
+        """
+        The trained weights for the average Perceptron Tagger on Part of Speech Tagging task.
+        """,
+        "https://github.com/JuliaText/TextAnalysis.jl/raw/2467ae2f379490af9ba1b181ce25f1a415a4be4d/src/pretrainedMod.bson",
+        "3305c8ee73d9de6d653d6a6e4eaf83dc5031114aaebe621b1625f11d7810a5f4",
+        post_fetch_method = function(fn)
+            file = readdir(".")[1]
+            println(readdir("."))
+            mv(file, "POSWeights.bson")
+        end
+    ))
+end
 
 """
 This file contains the Average Perceptron model and Perceptron Tagger
