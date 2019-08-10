24c8a0345e48b450011438c6a92ed96494cbda1f
Ayushk4 <ayushk4@gmail.com>
2019-08-10 16:09:45 +0530

    Tests for new NER APU over Docs and Corpus.

--- test/ner.jl
+++ test/ner.jl
@@ -19,4 +19,30 @@ using WordTokenizers
         str = "You owe John Doe 5¥."
         @test ner(str) ==  [ "O", "O", "PER", "PER", "O", "O", "O"]
     end
+
+    @testset "Documents and Corpus" begin
+        text1 = "We aRE vErY ClOSE tO ThE HEaDQuarTeRS."
+        text2 = "The World Health Organization (WHO) is a specialized agency of the United Nations that is concerned with international public health."
+
+        sd = StringDocument(text1)
+        td = TokenDocument(text2)
+
+        tags = ner(sd)
+        @test length(tags) == length(WordTokenizers.split_sentences(text1))
+        @test length(tags[1]) == length(WordTokenizers.tokenize(text1))
+        @test unique(vcat(tags...)) == ["O"]
+
+        tags = ner(td)
+        @test length(tags) == length(WordTokenizers.split_sentences(text2))
+        @test length(tags[1]) == length(WordTokenizers.tokenize(text2))
+        u =  unique(vcat(tags...))
+        @test "O" ∈ u && "ORG" ∈ u
+
+        crps = Corpus([sd, td])
+        tags = ner(crps)
+
+        @test length(tags) == length(crps.documents)
+        @test tags[1] == ner(crps.documents[1])
+        @test tags[2] == ner(crps.documents[2])
+    end
 end
