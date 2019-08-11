35d77194d306c19c4e027bdc0b1ab784ff13afa1
Ayushk4 <ayushk4@gmail.com>
2019-08-11 13:56:44 +0530

    Change the datadeps link for Avg perceptron tagger

--- src/averagePerceptronTagger.jl
+++ src/averagePerceptronTagger.jl
@@ -10,11 +10,12 @@ function pos_tagger_datadep_register()
         """
         The trained weights for the average Perceptron Tagger on Part of Speech Tagging task.
         """,
-        "https://github.com/JuliaText/TextAnalysis.jl/raw/2467ae2f379490af9ba1b181ce25f1a415a4be4d/src/pretrainedMod.bson",
-        "3305c8ee73d9de6d653d6a6e4eaf83dc5031114aaebe621b1625f11d7810a5f4",
+        "https://github.com/JuliaText/TextAnalysis.jl/releases/download/v0.6.0/pretrainedMod.bson.zip",
+        "52519cb3aea5d8f74368faedea831471e5df34567de4748d15decea7424743d3",
         post_fetch_method = function(fn)
-            file = readdir(".")[1]
-            println(readdir("."))
+            unpack(fn)
+            rm("__MACOSX", recursive=true)
+            file = readdir()[1]
             mv(file, "POSWeights.bson")
         end
     ))
