fc42fdfd092f9a7ae19dfbb530a511721e37a8c2
Ayushk4 <ayushk4@gmail.com>
2019-08-22 18:46:57 +0530

    Change the DataDeps path of POS weights

--- src/sequence/pos_datadeps.jl
+++ src/sequence/pos_datadeps.jl
@@ -3,7 +3,7 @@ function pos_datadep_register()
         """
         The weights for POS Sequence Labelling Model.
         """,
-        "https://github.com/Ayushk4/POS.jl/releases/download/v0.0/weights.tar.xz",
+        "https://github.com/JuliaText/TextAnalysis.jl/releases/download/v0.6.0/pos_weights.tar.xz",
         "b02e891ea913be6834ff67d6ecf2ddae6754d55509bb3d9c078dbfc7eed27988";
         post_fetch_method = function(fn)
             unpack(fn)
@@ -18,7 +18,7 @@ function pos_datadep_register()
         """
         The character and words dict for POS Sequence Labelling Model.
         """,
-        "https://github.com/Ayushk4/POS.jl/releases/download/v0.0/model_dicts.tar.xz",
+        "https://github.com/JuliaText/TextAnalysis.jl/releases/download/v0.6.0/pos_model_dicts.tar.xz",
         "4d7fe8238ff0cfb92d195dfa745b4ed08f916d4707e3dbe27a1b3144c9282f41";
         post_fetch_method = function(fn)
             unpack(fn)
