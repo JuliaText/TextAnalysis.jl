a1897487771e2076741b70eb379b954ff11db6dc
Ayushk4 <ayushk4@gmail.com>
2019-12-23 22:45:51 +0530

    Switch to JSON for Sequence Model Dicts, addressing 32-bit test fails.

--- src/sequence/ner_datadeps.jl
+++ src/sequence/ner_datadeps.jl
@@ -19,7 +19,7 @@ function ner_datadep_register()
         The character and words dict for NER Sequence Labelling Model.
         """,
         "https://github.com/Ayushk4/NER.jl/releases/download/0.0.0.1/ner_dicts.tar.xz",
-        "83c09a3020bece2cdbee8cf51c041b30318ecbaf7a9dfd895e6942bac0540bc7",
+        "190d8ed245f2e233d97c23c015ae1b3c11878974188d30c2fabceb281b7cbbda",
         post_fetch_method = function(fn)
             unpack(fn)
             dir = "model_dicts"
