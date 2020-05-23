698dc0e4876774b2238364a7f0bc234a11f14d90
tejasvaidhyadev <iamtejasvaidhya@gmail.com>
2020-05-24 04:06:48 +0530

    adding other language model

--- src/LM/langmodel.jl
+++ src/LM/langmodel.jl
@@ -1,19 +1,51 @@
 abstract type Langmodel end
 
-struct mle <: Langmodel
+struct MLE <: Langmodel
     vocab ::Vocabulary
     #counter::Dict{SubString{String},Array{Tuple{String,Float64},1}}
 end
 
-function mle(word,unk_cutoff=1 ,unk_label="<unk>")
-    mle(Vocabulary(word,unk_cutoff ,unk_label))
+function MLE(word,unk_cutoff=1 ,unk_label="<unk>")
+   MLE(Vocabulary(word,unk_cutoff ,unk_label))
 end
 
-function fit!(lm::Langmodel,text,min::Integer,max::Integer)
-    text = lookup(lm.vocab ,text)
-    text = convert(Array{String}, text)
-    return counter1(text,min,max)
+function (lm::MLE)(text,min::Integer,max::Integer) 
+     text = lookup(lm.vocab ,text)
+     text=convert(Array{String}, text)
+     return counter1(text,min,max,normalize)
 end
+
+struct Lidstone <: Langmodel
+    vocab ::Vocabulary
+    gamma ::Integer
+end
+
+function Lidstone(word,gamma,unk_cutoff=1 ,unk_label="<unk>")
+   Lidstone(Vocabulary(word,unk_cutoff ,unk_label),gamma)
+end
+
+function (lm::Lidstone)(text,min::Integer,max::Integer) 
+     text = lookup(lm.vocab ,text)
+     text=convert(Array{String}, text)
+     return counter1(text,min,max,lid_norm,gamma = lm.gamma)
+end
+
+
+struct Laplace <: Langmodel
+    vocab ::Vocabulary
+    gamma ::Integer
+end
+
+function Laplace(word,unk_cutoff=1 ,unk_label="<unk>")
+   Lidstone(Vocabulary(word,unk_cutoff ,unk_label),1)
+end
+
+function (lm::Laplace)(text,min::Integer,max::Integer) 
+     text = lookup(lm.vocab ,text)
+     text=convert(Array{String}, text)
+     return counter1(text,min,max,lid_norm,gamma = lm.gamma)
+end
+
 
 function unmaskscore(a::Dict{SubString{String},Array{Tuple{String,Float64},1}},word,context)
     for i in a[context]
