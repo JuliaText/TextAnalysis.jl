f240cae4aa2998495254fd8c94a13837eb56a001
tejasvaidhyadev <iamtejasvaidhya@gmail.com>
2020-05-30 21:16:17 +0530

    updating api

--- src/LM/api.jl
+++ src/LM/api.jl
@@ -7,6 +7,7 @@ end
 function logscore(m::Langmodel,temp_lm::DefaultDict,word,context)
     log2(maskedscore(m,temp_lm,word,context))
 end
+
 function entropy(m::Langmodel,lm::DefaultDict,text_ngram)
     local log_set=Float64[]
     for ngram in text_ngram
@@ -16,6 +17,7 @@ function entropy(m::Langmodel,lm::DefaultDict,text_ngram)
     end
     return(sum(log_set)/length(log_set))
 end
+
 function perplexity(m::Langmodel,lm::DefaultDict,text_ngram)
     return(2^(entropy(m,lm,text_ngram)))
 end
