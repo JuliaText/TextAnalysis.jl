5f4edf9b39f8aa16c2aa47f109ae0d7971ceeef6
Kim Buergl <kimbuergl@gmail.com>
2020-09-02 17:34:22 +0200

    Extend NaiveBayesClassifier to support Documents as input #152 (#219)
    NGramDocuments are currently not accepted and throw an error

--- test/bayes.jl
+++ test/bayes.jl
@@ -4,4 +4,11 @@
     TextAnalysis.fit!(m, "this is spam", :spam);
     r = TextAnalysis.predict(m, "is this spam?")
     @test r[:spam] > r[:ham]
+
+    n = NaiveBayesClassifier([:spam, :ham])
+    TextAnalysis.fit!(n, StringDocument("this is ham"), :ham)
+    TextAnalysis.fit!(n, StringDocument("this is spam"), :spam)
+    p = TextAnalysis.predict(n, TokenDocument("is this spam?"))
+    @test p[:spam] > p[:ham]
+    @test p[:spam] == r[:spam]
 end
