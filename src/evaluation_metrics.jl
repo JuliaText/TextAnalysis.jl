66d765735ab2230a6010b59fde134e8f78932f96
Roman S Samarev <rssdev10@gmail.com>
2023-09-12 09:30:23 -0700

    ROUGE: fixed sentences calculation and some minor refactoring

--- src/evaluation_metrics.jl
+++ src/evaluation_metrics.jl
@@ -9,11 +9,9 @@ See also: [`rouge_l_sentence`](@ref), [`rouge_l_summary`](@ref)
 """
 function rouge_n(references, candidate, n; avg = true, lang = Languages.English())
     ng_candidate = ngramize(lang, candidate, n)
-    ng_refs = [ngramize(lang, ref, n) for ref in references]
-
-    rouge_recall = Array{Float64,1}()
-    for ref in ng_refs
-        push!(rouge_recall, rouge_match_score(keys(ref), ng_candidate) / sum(values(ref)) )
+    rouge_recall = map(references) do ref
+        ng_ref = ngramize(lang, ref, n)
+        rouge_match_score(keys(ng_ref), ng_candidate) / sum(values(ng_ref))
     end
 
     avg == true && return jackknife_avg(rouge_recall)
@@ -22,9 +20,9 @@ end
 
 function rouge_match_score(ref, candidate::Dict)
     matches = 0
-    for p in keys(candidate)
+    for (p, v) in candidate
         p ∉ ref && continue
-        matches += candidate[p]
+        matches += v
     end
     return matches
 end
@@ -40,12 +38,12 @@ See also: [`rouge_n`](@ref), [`rouge_l_summary`](@ref)
 """
 function rouge_l_sentence(references, candidate, β=8, average = true)
     ngram_cand = tokenize(Languages.English(), candidate)
-    rouge_l_list = []
+    rouge_l_list = Float64[]
 
     for ref in references
         ngram_ref = tokenize(Languages.English(), ref)
-        r_lcs = weighted_lcs(ngram_ref, ngram_cand, true, false, sqrt) / length(ngram_ref)
-        p_lcs = weighted_lcs(ngram_ref, ngram_cand, true, false, sqrt) / length(ngram_cand)
+        r_lcs = weighted_lcs(ngram_ref, ngram_cand, true, sqrt) / length(ngram_ref)
+        p_lcs = weighted_lcs(ngram_ref, ngram_cand, true, sqrt) / length(ngram_cand)
         score = fmeasure_lcs(r_lcs, p_lcs, β)
         push!(rouge_l_list, score)
     end
@@ -66,7 +64,7 @@ See [Rouge: A package for automatic evaluation of summaries](http://www.aclweb.o
 See also: [`rouge_l_sentence()`](@ref), [`rouge_l_summary`](@ref)
 """
 function rouge_l_summary(references, candidate, β, averaging=true)
-    rouge_l_list = []
+    rouge_l_list = Float64[]
     cand_sent_list = split_sentences(candidate)
 
     for ref in references
@@ -75,11 +73,10 @@ function rouge_l_summary(references, candidate, β, averaging=true)
 
         for ref_sent in ref_sent_list
             l_ = []
-            arg1 = tokenize(Languages.English(), ref)
+            arg1 = tokenize(Languages.English(), ref_sent)
             for cand_sent in cand_sent_list
                 arg2 = tokenize(Languages.English(), cand_sent)
-                d = tokenize(Languages.English(), weighted_lcs(arg1, arg2, false, true, sqrt))
-                append!(l_,d)
+                append!(l_, weighted_lcs_tokens(arg1, arg2, false, sqrt))
             end
             sum_value += length(unique(l_))
         end
