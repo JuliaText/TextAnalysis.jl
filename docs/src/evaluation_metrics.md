14533f0ed92a93eb79dab821430b4ea541b78900
rssdev10 <rssdev10@gmail.com>
2023-10-23 19:41:49 -0700

    ROUGE: fixed sentences calculation and some minor refactoring (#272)
    ROUGE: 
    * fixed sentences calculation and some minor refactoring
    * changed output types, optimized, aligned with ROUGE-1.5.5 and checked with Google Research implementation
    * docs updates
    * rouge_l_sentence - added an argument with weighted function.

--- docs/src/evaluation_metrics.md
+++ docs/src/evaluation_metrics.md
@@ -8,36 +8,28 @@ As of now TextAnalysis provides the following evaluation metrics.
 
 * [BLEU (bilingual evaluation understudy)](https://en.wikipedia.org/wiki/BLEU)
 
-## ROUGE-N
+## ROUGE-N, ROUGE-L, ROUGE-L-Summary
 This metric evaluation based on the overlap of N-grams
 between the system and reference summaries.
 
 ```@docs
+argmax
+average
 rouge_n
+rouge_l_sentence
+rouge_l_summary
 ```
 
-The function takes the following arguments -
+```@example
+using TextAnalysis
 
-* `references::Array{T} where T<: AbstractString` = The list of reference summaries.
-* `candidate::AbstractString` = Input candidate summary, to be scored against reference summaries.
-* `n::Integer` = Order of NGrams
-* `avg::Bool` = Setting this parameter to `true`, applies jackkniving the calculated scores. Defaults to `true`
-* `lang::Language` = Language of the text, useful while generating N-grams. Defaults to English i.e. Languages.English()
+candidate_summary =  "Brazil, Russia, China and India are growing nations. They are all an important part of BRIC as well as regular part of G20 summits."
+reference_summaries = ["Brazil, Russia, India and China are the next big political powers in the global economy. Together referred to as BRIC(S) along with South Korea.", "Brazil, Russia, India and China are together known as the  BRIC(S) and have been invited to the G20 summit."]
 
-```julia
-julia> candidate_summary =  "Brazil, Russia, China and India are growing nations. They are all an important part of BRIC as well as regular part of G20 summits."
-"Brazil, Russia, China and India are growing nations. They are all an important part of BRIC as well as regular part of G20 summits."
-
-julia> reference_summaries = ["Brazil, Russia, India and China are the next big political powers in the global economy. Together referred to as BRIC(S) along with South Korea.", "Brazil, Russia, India and China are together known as the  BRIC(S) and have been invited to the G20 summit."]
-2-element Array{String,1}:
- "Brazil, Russia, India and China are the next big political powers in the global economy. Together referred to as BRIC(S) along with South Korea."
- "Brazil, Russia, India and China are together known as the  BRIC(S) and have been invited to the G20 summit."                                    
-
-julia> rouge_n(reference_summaries, candidate_summary, 2, avg=true)
-0.1317241379310345
-
-julia> rouge_n(reference_summaries, candidate_summary, 1, avg=true)
-0.5051282051282051
+results = [
+    rouge_n(reference_summaries, candidate_summary, 2),
+    rouge_n(reference_summaries, candidate_summary, 1)
+] .|> argmax
 ```
 
 ## BLEU (bilingual evaluation understudy)
--- src/utils.jl
+++ src/utils.jl
@@ -1,92 +1,67 @@
-# The jackknife is a resampling technique especially useful for variance and bias estimation.
-"""
-    jackknife_avg(`scores`)
-
-Apply jackknife on the input list of `scores`
-"""
-function jackknife_avg(scores)
-    if length(collect(Set(scores))) == 1
-        #= In case the elements of the array are all equal=#
-        return scores[1]
-    else
-        #=store the maximum scores
-        from the m different sets of m-1 scores.
-        such that m is the len(score_list)=#
-        average = []
-        for i in scores
-            # dummy : list a particular combo of m-1 scores
-            dummy = [j for j in scores if i != j]
-            append!(average, max(dummy...))
-            end
-
-        return sum(average)/length(average)
-    end
-end
 
 """
     weighted_lcs(X, Y, weight_score::Bool, returns_string::Bool, weigthing_function::Function)
 
 Compute the Weighted Longest Common Subsequence of X and Y.
 """
-function weighted_lcs(X, Y, weighted = true, returns_string = false, f = sqrt)
-    m, n = length(X), length(Y)
-    c_table = [zeros(n+1) for i in 1:m+1]
-    w_table = [zeros(n+1) for i in 1:m+1]
+function weighted_lcs(X, Y, weighted=true, f=sqrt)
+    result = weighted_lcs_inner(X, Y, weighted, f)
 
-    for i in 2:(m+1)
-        for j in 2:(n+1)
-            if X[i-1] == Y[j-1]
-                k = w_table[i-1][j-1]
-                if weighted == true
-                    increment = (f(k+1)) - (f(k))
-                else
-                    increment = 1
-                end
-                c_table[i][j] = c_table[i-1][j-1] + increment
-                w_table[i][j] = k + 1
-            else
-                if c_table[i-1][j] > c_table[i][j-1]
-                    c_table[i][j] = c_table[i-1][j]
-                    w_table[i][j] = 0  # no match at i,j
-                else
-                    c_table[i][j] = c_table[i][j-1]
-                    w_table[i][j] = 0  # no match at i,j
-                end
-            end
-        end
-    end
+    return result.c_table[end, end]
+end
 
-    lcs_length = (c_table[m+1][n+1])
-    if returns_string == false
-        return lcs_length
-    end
+function weighted_lcs_tokens(X, Y, weighted=true, f=sqrt)
+    m, n, c_table, _w_table = weighted_lcs_inner(X, Y, weighted, f)
 
-    if weighted == true
-        lcs_length = c_table[m][n]^(2)
-    end
-
-    lcs_length = round(Int64, lcs_length)
-    lcs_length = convert(Int64, lcs_length)
-    lcs = ["" for i in  1:(lcs_length+1)]
-    lcs[lcs_length+1] = ""
+    # if weighted == true 
+    #     lcs_length = c_table[m, n]^(2) # ?....
+    # end
+    lcs_length = m
+    lcs = String["" for i in 1:(lcs_length+1)]
     i = m + 1
     j = n + 1
 
-    while i>1 && j>1
+    while i > 1 && j > 1
         if X[i-1] == Y[j-1]
             lcs[lcs_length+1] = X[i-1]
             i -= 1
             j -= 1
             lcs_length -= 1
-        elseif c_table[i-1][j] > c_table[i][j-1]
+        elseif c_table[i-1, j] > c_table[i, j-1]
             i -= 1
         else
             j -= 1
         end
     end
 
-    return (join(lcs, " "))  # the lcs string
+    return lcs  # the lcs string
 end
+
+function weighted_lcs_inner(X, Y, weighted=true, f=sqrt)
+    m, n = length(X), length(Y)
+    c_table = zeros(Float32, m + 1, n + 1)
+    w_table = zeros(Int32, m + 1, n + 1)
+    increment = 1
+
+    for i in 2:(m+1)
+        for j in 2:(n+1)
+            if X[i-1] == Y[j-1]
+                k = w_table[i-1, j-1]
+                if weighted == true
+                    increment = (f(k + 1)) - (f(k))
+                end
+                c_table[i, j] = c_table[i-1, j-1] + increment
+                w_table[i, j] = k + 1
+            else
+                c_table[i, j] = max(c_table[i-1, j], c_table[i, j-1])
+                # w_table[i, j] = 0  # no match at i,j
+            end
+        end
+    end
+
+    (m=m, n=n, c_table=c_table, w_table=w_table)
+end
+
 
 """
     fmeasure_lcs(RLCS, PLCS, β)
@@ -99,14 +74,7 @@ Compute the F-measure based on WLCS.
 - `PLCS` - Precision Factor
 - `β` - Parameter
 """
-function fmeasure_lcs(RLCS, PLCS, β=1)
-    try
-        return ((1 + β ^ 2) * RLCS * PLCS) / (RLCS + (β ^ 2) * PLCS)
-    catch ex
-        if ex isa DivideError
-            return 0
-        else
-            rethrow(ex)
-        end
-    end
+function fmeasure_lcs(RLCS::Real, PLCS::Real, β=1.0)::Real
+    divider = RLCS + (β^2) * PLCS
+    return iszero(divider) ? 0.0 : (1 + β^2) * RLCS * PLCS / divider
 end
