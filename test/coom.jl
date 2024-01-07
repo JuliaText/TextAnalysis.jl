48d2e5e5e42fcd8b795f388e9debe578810ce105
atantos <atantos@gmail.com>
2024-01-07 02:12:44 +0200

    Added tests

--- test/coom.jl
+++ test/coom.jl
@@ -36,7 +36,8 @@
             @test TextAnalysis.coom(C) == expected_result_C
             @test TextAnalysis.coom(D) == expected_result_D
         else
-            @test TextAnalysis.coom(C) == length(crps) * expected_result
+            @test TextAnalysis.coom(C) == length(crps) * expected_result_C
+            @test TextAnalysis.coom(D) == length(crps) * expected_result_D
         end
     end
     @test_throws ErrorException TextAnalysis.CooMatrix(nd)
@@ -45,28 +46,53 @@
     terms = tokens(td)
     for d in [sd, td, crps]
         C = TextAnalysis.CooMatrix{T}(d, terms)
+        D = TextAnalysis.CooMatrix{T}(d, terms, mode=:directional)
         @test C isa TextAnalysis.CooMatrix{T}
         if !(d isa Corpus)
-            @test TextAnalysis.coom(C) == T.(expected_result)
+            @test TextAnalysis.coom(C) == T.(expected_result_C)
+            @test TextAnalysis.coom(D) == T.(expected_result_D)
         else
-            @test TextAnalysis.coom(C) == length(crps) * T.(expected_result)
+            @test TextAnalysis.coom(C) == length(crps) * T.(expected_result_C)
+            @test TextAnalysis.coom(D) == length(crps) * T.(expected_result_D)
         end
     end
     @test_throws ErrorException TextAnalysis.CooMatrix{T}(nd)
 
     # Results for window = 1, custom terms
     terms = ["this", "document", "it"]
-    expected_result = [0.0 0.0 0.0; # document
+    expected_result_C = [0.0 0.0 0.0; # document
         0.0 0.0 2.0; # it
         0.0 2.0 0.0] # this
+
+    expected_result_D = [0.0 0.0 0.0; # document
+        0.0 0.0 1.0; # it
+        0.0 1.0 0.0] # this
+
     # Verify untyped constructor
     for d in [sd, td, crps]
+        C = TextAnalysis.CooMatrix(d, terms, window=1)
+        D = TextAnalysis.CooMatrix(d, terms, window=1, mode=:directional)
+        if !(d isa Corpus)
+            @test TextAnalysis.coom(C) == T.(expected_result_C)
+            @test TextAnalysis.coom(D) == T.(expected_result_D)
+        else
+            @test TextAnalysis.coom(C) == length(crps) * T.(expected_result_C)
+            @test TextAnalysis.coom(D) == length(crps) * T.(expected_result_D)
+        end
+    end
+    @test_throws ErrorException TextAnalysis.CooMatrix(nd)
+
+    # Verify typed constructor
+    for d in [sd, td, crps]
         C = TextAnalysis.CooMatrix{T}(d, terms, window=1)
+        D = TextAnalysis.CooMatrix{T}(d, terms, window=1, mode=:directional)
         @test C isa TextAnalysis.CooMatrix{T}
         if !(d isa Corpus)
-            @test TextAnalysis.coom(C) == T.(expected_result)
+            @test TextAnalysis.coom(C) == T.(expected_result_C)
+            @test TextAnalysis.coom(D) == T.(expected_result_D)
         else
-            @test TextAnalysis.coom(C) == length(crps) * T.(expected_result)
+            @test TextAnalysis.coom(C) == length(crps) * T.(expected_result_C)
+            @test TextAnalysis.coom(D) == length(crps) * T.(expected_result_D)
         end
     end
     @test_throws ErrorException TextAnalysis.CooMatrix{T}(nd)
