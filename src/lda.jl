9106d9a0a4e58d7326cb2ecafef9d2af9d1cc364
Hossein Pourbozorg <prbzrg@gmail.com>
2024-08-18 07:59:48 +0000

    Use `enabled=showprogress`

--- src/lda.jl
+++ src/lda.jl
@@ -51,60 +51,55 @@ function lda(
 
     for wordid in 1:number_of_words
         nzdocs_idxs = nzrange(dtm.dtm, wordid)
-        for docid in dtm.dtm.rowval[nzdocs_idxs]
-            for _ in 1:dtm.dtm[docid, wordid]
-                topicid = rand(1:ntopics)
-                update_target_topic = topics[topicid]
-                update_target_topic.count += 1
-                update_target_topic.wordcount[wordid] = get(update_target_topic.wordcount, wordid, 0) + 1
-                topics[topicid] = update_target_topic
-                topic_base_document = docs[docid]
-                push!(topic_base_document.topic, topicid)
-                push!(topic_base_document.text, wordid)
-                topic_base_document.topicidcount[topicid] += 1
-            end
+        for docid in dtm.dtm.rowval[nzdocs_idxs],
+                _ in 1:dtm.dtm[docid, wordid]
+            topicid = rand(1:ntopics)
+            update_target_topic = topics[topicid]
+            update_target_topic.count += 1
+            update_target_topic.wordcount[wordid] = get(update_target_topic.wordcount, wordid, 0) + 1
+            topics[topicid] = update_target_topic
+            topic_base_document = docs[docid]
+            push!(topic_base_document.topic, topicid)
+            push!(topic_base_document.text, wordid)
+            topic_base_document.topicidcount[topicid] += 1
         end
     end
 
     probs = Vector{Float64}(undef, ntopics)
 
-    wait_time = showprogress ? 1.0 : Inf
-
     # Gibbs sampling
-    @showprogress dt = wait_time for _ in 1:iteration
-        for doc in docs
-            for (i, word) in enumerate(doc.text)
-                topicid_current = doc.topic[i]
-                doc.topicidcount[topicid_current] -= 1
-                topics[topicid_current].count -= 1
-                topics[topicid_current].wordcount[word] -= 1
-                document_lenth = length(doc.text) - 1
+    @showprogress enabled=showprogress for _ in 1:iteration,
+            doc in docs,
+            (i, word) in enumerate(doc.text)
+        topicid_current = doc.topic[i]
+        doc.topicidcount[topicid_current] -= 1
+        topics[topicid_current].count -= 1
+        topics[topicid_current].wordcount[word] -= 1
+        document_lenth = length(doc.text) - 1
 
-                for target_topicid in 1:ntopics
-                    topicprob = (doc.topicidcount[target_topicid] + beta) / (document_lenth + beta * ntopics)
-                    topic = topics[target_topicid]
-                    wordprob = (get(topic.wordcount, word, 0) + alpha) / (topic.count + alpha * number_of_words)
-                    probs[target_topicid] = topicprob * wordprob
-                end
-                normalize_probs = sum(probs)
+        for target_topicid in 1:ntopics
+            topicprob = (doc.topicidcount[target_topicid] + beta) / (document_lenth + beta * ntopics)
+            topic = topics[target_topicid]
+            wordprob = (get(topic.wordcount, word, 0) + alpha) / (topic.count + alpha * number_of_words)
+            probs[target_topicid] = topicprob * wordprob
+        end
+        normalize_probs = sum(probs)
 
-                # select new topic
-                select = rand()
-                sum_of_prob = 0.0
-                new_topicid = 1
-                for (selected_topicid, prob) in enumerate(probs)
-                    sum_of_prob += prob / normalize_probs
-                    if select < sum_of_prob
-                        new_topicid = selected_topicid
-                        break
-                    end
-                end
-                doc.topic[i] = new_topicid
-                doc.topicidcount[new_topicid] = get(doc.topicidcount, new_topicid, 0) + 1
-                topics[new_topicid].count += 1
-                topics[new_topicid].wordcount[word] = get(topics[new_topicid].wordcount, word, 0) + 1
+        # select new topic
+        select = rand()
+        sum_of_prob = 0.0
+        new_topicid = 1
+        for (selected_topicid, prob) in enumerate(probs)
+            sum_of_prob += prob / normalize_probs
+            if select < sum_of_prob
+                new_topicid = selected_topicid
+                break
             end
         end
+        doc.topic[i] = new_topicid
+        doc.topicidcount[new_topicid] = get(doc.topicidcount, new_topicid, 0) + 1
+        topics[new_topicid].count += 1
+        topics[new_topicid].wordcount[word] = get(topics[new_topicid].wordcount, word, 0) + 1
     end
 
     # ϕ
